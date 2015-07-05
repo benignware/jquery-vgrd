70c3021a6843819c3627f0a292eef42223713437
Rafael Nowrotek <mail@benignware.com>
2015-07-05 22:39:50 +0200

    Added grunt-livemd

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,9 @@ module.exports = function(grunt) {
     copy: {
       dist: {
         expand: true, cwd: 'src/', src: ['**'], dest: 'dist/'
+      },
+      site: {
+        expand: true, cwd: 'dist/', src: ['**/*'], dest: 'site/'
       }
     }, 
     uglify: {
@@ -16,12 +19,39 @@ module.exports = function(grunt) {
           'dist/jquery.vgrd.min.js': [ 'dist/jquery.vgrd.js']
         }
       }
+    },
+    livemd: {
+      options: {
+      },
+      site: {
+        files: {
+          'site/index.html': ['README.md']
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        // Options for all targets go here.
+      },
+      push: {
+        options: {
+          base: 'site'
+        },
+        // These files will get pushed to the `gh-pages` branch (the default).
+        src: ['index.html', 'jquery.vgrd.min.js']
+      }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-livemd');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('default', ['copy', 'uglify']);
+  grunt.registerTask('default', ['copy:dist', 'jshint', 'uglify']);
+  
+  grunt.registerTask('build', ['copy:dist', 'uglify']);
+  
+  grunt.registerTask('site', ['build', 'copy:site', 'livemd:site']);
   
 };
